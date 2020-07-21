480ca08fd66a98161534d996689fe303f12300c8
Reza Hosseini <srh199372@gmail.com>
2020-07-21 17:36:54 +0430

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,51 @@
+// Animations
+const items = document.querySelectorAll(".section-a li");
+const items2 = document.querySelectorAll(".skills-bars .bar");
+
+const isInViewport = (el) => {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+const run = () =>
+  items.forEach((item) => {
+    if (isInViewport(item)) {
+      item.classList.add("show");
+    }
+  });
+const run2 = () =>
+  items2.forEach((item) => {
+    if (isInViewport(item)) {
+      item.classList.add("show");
+    }
+  });
+
+// Events
+window.addEventListener("load", run);
+window.addEventListener("resize", run);
+window.addEventListener("scroll", run);
+window.addEventListener("load", run2);
+window.addEventListener("resize", run2);
+window.addEventListener("scroll", run2);
+
+// Smooth Scrolling
+$("#menu a").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800
+    );
+  }
+});
